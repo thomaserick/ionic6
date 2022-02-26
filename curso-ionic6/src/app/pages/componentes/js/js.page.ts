f9497887b2059647b1644c623cc1473e40616efd
Thomas Erick <thomaserick.dev@gmail.com>
2022-02-26 12:00:08 -0300

    component js

--- curso-ionic6/src/app/pages/componentes/js/js.page.ts
+++ curso-ionic6/src/app/pages/componentes/js/js.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./js.page.scss'],
 })
 export class JsPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
   }
-
 }
