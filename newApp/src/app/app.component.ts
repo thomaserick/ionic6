dc716dbc2d73ca77effdda5678cd3d9aaa97e318
Thomas Erick <thomaserick.dev@gmail.com>
2022-06-14 22:25:57 -0300

    Alterando cor

--- newApp/src/app/app.component.ts
+++ newApp/src/app/app.component.ts
@@ -5,16 +5,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-    { title: 'Home', url: '/home', icon: 'warning' },
-    { title: 'Custumers', url: '/custumers', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public appPages = [   
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Custumers', url: '/custumers', icon: 'people' },
+  ]; 
   constructor() {}
 }
